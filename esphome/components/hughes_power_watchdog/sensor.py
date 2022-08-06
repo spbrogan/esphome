bdb1cda3f168db1ab9ffdde459ca65bef2c75b59
Sean Brogan <spbrogan@live.com>
2022-08-05 19:12:05 -0700

    Initial attempt to add support for Hughes Power Watchdog 50A BLE surge protector
    Derivied from Ble Text Sensor and airthings wave mini as examples.

---
+++ esphome/components/hughes_power_watchdog/sensor.py
@@ -0,0 +1,135 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, ble_client, esp32_ble_tracker
+from esphome.const import (
+    CONF_ID,
+    CONF_VOLTAGE,
+    CONF_CURRENT,
+    CONF_POWER,
+    CONF_TOTAL_POWER,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_VOLTAGE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_AMPERE,
+    UNIT_VOLT,
+    UNIT_WATT
+)
+_CONF_LINE_1 = "_line_1"
+_CONF_LINE_2 = "_line_2"
+
+CONF_VOLTAGE_LINE_1 = CONF_VOLTAGE + _CONF_LINE_1
+CONF_CURRENT_LINE_1 = CONF_CURRENT + _CONF_LINE_1
+CONF_POWER_LINE_1 = CONF_POWER + _CONF_LINE_1
+
+CONF_VOLTAGE_LINE_2 = CONF_VOLTAGE + _CONF_LINE_2
+CONF_CURRENT_LINE_2 = CONF_CURRENT + _CONF_LINE_2
+CONF_POWER_LINE_2 = CONF_POWER + _CONF_LINE_2
+
+
+
+ICON_PROPANE_TANK = "mdi:propane-tank"
+
+
+CODEOWNERS = ["@spbrogan"]
+DEPENDENCIES = ["ble_client"]
+
+hughes_power_watchdog_ns = cg.esphome_ns.namespace("hughes_power_watchdog")
+HughesPowerWatchdog = hughes_power_watchdog_ns.class_(
+    "HughesPowerWatchdog", ble_client.BLEClientNode, cg.Component
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(HughesPowerWatchdog),
+            cv.Optional(CONF_VOLTAGE_LINE_1): sensor.sensor_schema(
+                unit_of_measurement=UNIT_VOLT,
+                #icon=ICON_THERMOMETER,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_CURRENT_LINE_1): sensor.sensor_schema(
+                unit_of_measurement=UNIT_AMPERE,
+                #icon=ICON_PROPANE_TANK,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_CURRENT,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_POWER_LINE_1): sensor.sensor_schema(
+                unit_of_measurement=UNIT_WATT,
+                #icon=ICON_RULER,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_VOLTAGE_LINE_2): sensor.sensor_schema(
+                unit_of_measurement=UNIT_VOLT,
+                #icon=ICON_THERMOMETER,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_CURRENT_LINE_2): sensor.sensor_schema(
+                unit_of_measurement=UNIT_AMPERE,
+                #icon=ICON_PROPANE_TANK,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_CURRENT,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_POWER_LINE_2): sensor.sensor_schema(
+                unit_of_measurement=UNIT_WATT,
+                #icon=ICON_RULER,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_TOTAL_POWER): sensor.sensor_schema(
+                unit_of_measurement=UNIT_WATT,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await ble_client.register_ble_node(var, config)
+
+    if CONF_VOLTAGE_LINE_1 in config:
+        sens = await sensor.new_sensor(config[CONF_VOLTAGE_LINE_1])
+        cg.add(var.set_voltage_line_1(sens))
+
+    if CONF_CURRENT_LINE_1 in config:
+        sens = await sensor.new_sensor(config[CONF_CURRENT_LINE_1])
+        cg.add(var.set_current_line_1(sens))
+
+    if CONF_POWER_LINE_1 in config:
+        sens = await sensor.new_sensor(config[CONF_POWER_LINE_1])
+        cg.add(var.set_power_line_1(sens))
+
+    if CONF_VOLTAGE_LINE_2 in config:
+        sens = await sensor.new_sensor(config[CONF_VOLTAGE_LINE_2])
+        cg.add(var.set_voltage_line_2(sens))
+
+    if CONF_CURRENT_LINE_2 in config:
+        sens = await sensor.new_sensor(config[CONF_CURRENT_LINE_2])
+        cg.add(var.set_current_line_2(sens))
+
+    if CONF_POWER_LINE_2 in config:
+        sens = await sensor.new_sensor(config[CONF_POWER_LINE_2])
+        cg.add(var.set_power_line_2(sens))
+
+    if CONF_TOTAL_POWER in config:
+        sens = await sensor.new_sensor(config[CONF_TOTAL_POWER])
+        cg.add(var.set_cumulative_energy(sens))
+    
+
+    
