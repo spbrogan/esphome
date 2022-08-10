68cf5d39990b6873989a6b00ab9f49b7dc052853
Sean Brogan <spbrogan@live.com>
2022-08-09 23:45:12 -0700

    missed declaring override of update function

--- esphome/components/hughes_power_watchdog/hughes_power_watchdog.h
+++ esphome/components/hughes_power_watchdog/hughes_power_watchdog.h
@@ -21,6 +21,7 @@ class HughesPowerWatchdog : public PollingComponent, public ble_client::BLEClien
  public:
   HughesPowerWatchdog();
   void dump_config() override;
+  void update() override;
 
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override;
