5e5a5ab3807e489a327d908f7fbf91e800ad6aac
Sean Brogan <spbrogan@live.com>
2022-08-13 23:06:33 -0700

    track new data and connected status

--- esphome/components/hughes_power_watchdog/hughes_power_watchdog.h
+++ esphome/components/hughes_power_watchdog/hughes_power_watchdog.h
@@ -32,14 +32,14 @@ class HughesPowerWatchdog : public PollingComponent, public ble_client::BLEClien
   void set_current_line_1(sensor::Sensor *current) { current_l1_ = current; };  // amps
   void set_current_line_2(sensor::Sensor *current) { current_l2_ = current; };  // amps
 
-  void set_power_line_1(sensor::Sensor *power) { power_l1_ = power; };  // watts
-  void set_power_line_2(sensor::Sensor *power) { power_l2_ = power; };  // watts
+  void set_power_line_1(sensor::Sensor *power) { power_l1_ = power; };          // watts
+  void set_power_line_2(sensor::Sensor *power) { power_l2_ = power; };          // watts
   void set_power_combined(sensor::Sensor *power) { power_combined_ = power; };  // watts
 
   void set_cumulative_energy(sensor::Sensor *energy) { cumulative_energy_ = energy; };  // kilowatt hours
 
-  void set_error_code(sensor::Sensor *error) { error_code_ = error; }; // error code between 0 - 9
-  void set_error_text(text_sensor::TextSensor *error_text) { error_text_ = error_text; }; //report the error message as text
+  void set_error_code(sensor::Sensor *error) { error_code_ = error; };  // error code between 0 - 9
+  void set_error_text(text_sensor::TextSensor *error_text) { error_text_ = error_text; };  // error message text
   uint16_t handle;
 
  protected:
@@ -54,21 +54,23 @@ class HughesPowerWatchdog : public PollingComponent, public ble_client::BLEClien
   sensor::Sensor *cumulative_energy_{nullptr};
   sensor::Sensor *error_code_{nullptr};
   text_sensor::TextSensor *error_text_{nullptr};
+  void ReportSensor(bool UseInstanceData);
 
   uint8_t chunk_1_content_populated;
   esp32_ble_tracker::ESPBTUUID service_uuid_;
   esp32_ble_tracker::ESPBTUUID char_uuid_;
 
   uint8_t msg_buffer_[40];
-  float line1_v_;  // line 1 volts
-  float line2_v_;  // line 2 volts
-  float line1_c_;  // line 1 current
-  float line2_c_;  // line 2 current
-  float line1_p_;  // line 1 power
-  float line2_p_;  // line 2 power
-  float line1_ce_; // line 1 cumulative energy
-  float line2_ce_; // line 2 cumulative energy
-  uint8_t error_code_value_;
+  float line1_v_;             // line 1 volts
+  float line2_v_;             // line 2 volts
+  float line1_c_;             // line 1 current
+  float line2_c_;             // line 2 current
+  float line1_p_;             // line 1 power
+  float line2_p_;             // line 2 power
+  float line1_ce_;            // line 1 cumulative energy
+  float line2_ce_;            // line 2 cumulative energy
+  uint8_t error_code_value_;  // value of
+  bool new_data_;             // boolean to indicate new data
 };
 
 }  // namespace hughes_power_watchdog
